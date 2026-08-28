2027b83d0d8bbb6de64a8a8c1885dae6d55ce188
vega2krookies6 <vega2krookies6@gmail.com>
2026-08-28 14:46:42 +0900

    UserRepository add

---
+++ src/main/java/mylab/user/di/annot/UserRepository.java
@@ -0,0 +1,20 @@
+package mylab.user.di.annot;
+
+public class UserRepository {
+   private String dbType;
+    
+    public UserRepository() {}
+    
+    public String getDbType() { return dbType; }
+    public void setDbType(String dbType) { this.dbType = dbType; }
+    
+    public boolean saveUser(String userId, String name) {
+        System.out.println("사용자 저장: " + userId + ", " + name + " (DB: " + dbType + ")");
+        return true;
+    }
+    
+    @Override
+    public String toString() {
+        return "UserRepository [dbType=" + dbType + "]";
+    }
+}
