2d3fe1339d2b1d092b02562925ba22509cf194c6
vega2krookies6 <vega2krookies6@gmail.com>
2026-08-28 10:29:54 +0900

    HelloBeanCons add

--- src/test/java/myspring/di/annot/AnnotatedHelloBeanTest.java
+++ src/test/java/myspring/di/annot/AnnotatedHelloBeanTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import javax.annotation.Resource;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,7 +20,18 @@ public class AnnotatedHelloBeanTest {
 	@Resource(name = "stringPrinter")
 	PrinterBean printer;
 	
+	@Autowired
+	HelloBeanCons helloCons;
+	
+	//전략2 어노테이션 방식에서의 Constructor Injection 테스트
 	@Test
+	void helloBeansCons() {
+		assertEquals("Hello 어노테이션생성자", helloCons.sayHello());
+	}
+	
+	
+	//전략2 어노테이션 방식에서의  Setter Injection을 테스트
+	@Test @Disabled
 	void helloBeanAnnot() {
 		assertEquals("Hello 어노테이션", hello.sayHello());
 		hello.print();
