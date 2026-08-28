0ec077a8a4ca53885c2f61d36363ce90a394efed
vega2krookies6 <vega2krookies6@gmail.com>
2026-08-28 14:48:13 +0900

    UserService add

---
+++ src/main/java/mylab/user/di/annot/UserService.java
@@ -0,0 +1,17 @@
+package mylab.user.di.annot;
+public class UserService {
+	
+    private UserRepository userRepository;
+        
+    private SecurityService securityService;
+        
+    public UserRepository getUserRepository() { return userRepository; }
+    public SecurityService getSecurityService() { return securityService; }
+    
+    public boolean registerUser(String userId, String name, String password) {
+        if (securityService.authenticate(userId, password)) {
+            return userRepository.saveUser(userId, name);
+        }
+        return false;
+    }
+}
