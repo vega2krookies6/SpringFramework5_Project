9f1d9c147ac17b0d5c94cbc237e07266dd47d3f0
vega2krookies6 <vega2krookies6@gmail.com>
2026-08-28 10:17:54 +0900

    HelloBeanCons add

---
+++ src/main/java/myspring/di/annot/HelloBeanCons.java
@@ -0,0 +1,58 @@
+package myspring.di.annot;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class HelloBeanCons {
+	String name;
+	
+	PrinterBean printer;
+	
+	List<String> names;
+
+	public HelloBeanCons() {
+		System.out.println(this.getClass().getName() + " 기본생성자 호출됨!");
+	}
+
+	@Autowired
+	public HelloBeanCons(@Value("어노테이션생성자") String name, 
+			             @Qualifier("consolePrinter") PrinterBean printer) {
+		System.out.println(this.getClass().getName() + " 오버로딩 생성자 호출됨!");
+		this.name = name;
+		this.printer = printer;
+//		setName(name);
+//		setPrinter(printer);
+	}
+
+	public List<String> getNames() {
+		return this.names;
+	}
+
+	public void setNames(List<String> list) {
+		this.names = list;
+	}
+
+//	public void setName(String name) {
+//		System.out.println("setName() 메서드 호출됨 " + name);
+//		this.name = name;
+//	}
+//
+//	public void setPrinter(PrinterBean printer) {
+//		System.out.println("setPrinter() 메서드 호출됨 " + printer.getClass().getName());
+//		this.printer = printer;
+//	}
+
+	public String sayHello() {
+		return "Hello " + name;
+	}
+
+	public void print() {
+		this.printer.print(sayHello());
+	}
+
+}
