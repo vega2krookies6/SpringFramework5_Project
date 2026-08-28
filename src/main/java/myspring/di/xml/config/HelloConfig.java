c7cbdddc6f8db8de12daf6d146f0ec00f6417553
vega2krookies6 <vega2krookies6@gmail.com>
2026-08-28 14:10:20 +0900

    HelloConfig add

--- src/main/java/myspring/di/xml/config/HelloConfig.java
+++ src/main/java/myspring/di/xml/config/HelloConfig.java
@@ -1,15 +1,21 @@
 package myspring.di.xml.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 
+import myspring.di.xml.ConsolePrinter;
+import myspring.di.xml.Hello;
 import myspring.di.xml.Printer;
 import myspring.di.xml.StringPrinter;
 
 @Configuration
 @PropertySource(value="classpath:values.properties")
 public class HelloConfig {
+	@Autowired
+	Environment env;
 	
 	//<bean id="strPrinter" class="myspring.di.xml.StringPrinter" />
 	@Bean
@@ -17,6 +23,26 @@ public class HelloConfig {
 		return new StringPrinter();
 	}
 	
+	//<bean id="conPrinter" class="myspring.di.xml.ConsolePrinter" />
+	@Bean
+	public Printer consolePrinter() {
+		return new ConsolePrinter();
+	}
+	/*
+	 * 	<bean id="hello" class="myspring.di.xml.Hello" scope="singleton">
+			<!-- setName("스프링") 메서드 호출 -->
+			<property name="name" value="스프링" />
+			<!-- setPrinter(new StringPrinter()) 메서드 호출 -->
+			<property name="printer" ref="strPrinter" />
+	    </bean>
+	*/
+	@Bean
+	public Hello hello() {
+		Hello hello = new Hello();
+		String propName = env.getProperty("myName3");
+		hello.setName(propName);
+		return hello;
+	}
 	
 
 }
