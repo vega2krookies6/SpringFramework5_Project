fd650b3fbb0639a66b16f11a0075f1e1661b47f7
vega2krookies6 <vega2krookies6@gmail.com>
2026-08-28 09:52:15 +0900

    AnnotatedHelloBeanTest update

--- src/test/java/myspring/di/annot/AnnotatedHelloBeanTest.java
+++ src/test/java/myspring/di/annot/AnnotatedHelloBeanTest.java
@@ -2,12 +2,22 @@ package myspring.di.annot;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(locations = "classpath:hello-annot.xml")
 public class AnnotatedHelloBeanTest {
-
+	@Autowired
+	HelloBean hello;
+	
+	@Test
+	void helloBeanAnnot() {
+		assertEquals("어노테이션", hello.sayHello());
+	}
+	
+	
 }
